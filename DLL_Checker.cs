7dad7b93d36d706f55a532af3f94b9a592288115
Sphelele Mkhwanazi <spheleletholy262000@gmail.com@users.noreply.github.com>
2023-05-12 16:24:31 +0200

    Add project files.

---
+++ DLL_Checker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL_Version_Checker
+{
+    public class DLL_Checker
+    {
+        public string Name { get; }
+        public string FilePath { get; }
+        public List<Version> Versions { get; }
+
+        public DLL_Checker(string name, string filePath)
+        {
+            Name = name;
+            FilePath = filePath;
+            Versions = new List<Version>();
+
+            // Attempt to load the library into an assembly object
+            var assembly = Assembly.LoadFile(filePath);
+
+            // Get the version number of the library from its assembly
+            var version = assembly.GetName().Version;
+
+            // Add the library's version number to its Versions list
+            Versions.Add(version);
+        }
+
+        public Version AverageVersion()
+        {
+            return new Version(
+                (int)Versions.Average(x => x.Major),
+                (int)Versions.Average(x => x.Minor),
+                (int)Versions.Average(x => x.Build),
+                (int)Versions.Average(x => x.Revision)
+            );
+        }
+
+        public List<Version> OutdatedVersions(Version averageVersion)
+        {
+            return Versions.Where(x => x < averageVersion).ToList();
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DLL_Version_Checker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        private readonly List<string> exclusions = new List<string> { "System", "Microsoft", "mscorlib" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void checkButton_Click(object sender, RoutedEventArgs e)
+        {
+          
+            libraryList.Items.Clear();
+
+            
+            var libraryDirectory = @"C:\Users\User\source\repos\TestApp1\bin\Release\net6.0";
+
+            
+            var libraryFiles = Directory.GetFiles(libraryDirectory, "*.dll");
+
+            
+            var libraries = new List<DLL_Checker>();
+
+            
+            foreach (var libraryFile in libraryFiles)
+            {
+                
+                var libraryName = System.IO.Path.GetFileNameWithoutExtension(libraryFile);
+
+              
+                if (exclusions.Any(x => libraryName.StartsWith(x)))
+                    continue;
+
+                try
+                {
+                    
+                    var library = new DLL_Checker(libraryName, libraryFile);
+                    libraries.Add(library);
+                }
+                catch (Exception ex)
+                {
+                    
+                    libraryList.Items.Add($"Error checking {libraryName}: {ex.Message}");
+                }
+            }
+
+            
+            foreach (var library in libraries)
+            {
+                
+                var averageVersion = library.AverageVersion();
+
+                
+                var outdatedVersions = library.OutdatedVersions(averageVersion);
+
+               
+                if (outdatedVersions.Count > 0)
+                {
+                    libraryList.Items.Add($"{library.Name} ({outdatedVersions.Count} outdated versions)");
+                    foreach (var version in outdatedVersions)
+                    {
+                        libraryList.Items.Add($"\t{version}");
+                    }
+                }
+            }
+        }
+    }
+      
+}
